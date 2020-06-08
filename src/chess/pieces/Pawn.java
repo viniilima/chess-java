befba79fc7e71b6e2dc289efe1198cb7c0e3402a
Vinicius Assis <vinii.lima@outlook.com>
2020-06-08 12:27:27 -0300

    Special move - En Passant

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Pawn extends ChessPiece {
 
-	public Pawn(Board board, Color color) {
+	private ChessMatch match;
+	
+	public Pawn(Board board, Color color, ChessMatch match) {
 		super(board, color);
+		this.match = match;
 	}
 	
 	@Override
@@ -43,6 +47,17 @@ public class Pawn extends ChessPiece {
 			p.setValues(position.getRow() - 1, position.getColumn() + 1);
 			if(getBoard().positionExists(p) && isThereOpponentPiece(p))
 				m[p.getRow()][p.getColumn()] = true;
+			
+			// #specialmove en passant white
+			if(position.getRow() == 3) {
+				Position left = new Position(position.getRow(), position.getColumn() - 1);
+				if(getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().getPiece(left) == match.getEnPassant())
+					m[left.getRow() - 1][left.getColumn()] = true;
+				
+				Position right = new Position(position.getRow(), position.getColumn() + 1);
+				if(getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().getPiece(right) == match.getEnPassant())
+					m[right.getRow() - 1][right.getColumn()] = true;
+			}
 		}
 		else {
 			// 1f
@@ -65,6 +80,17 @@ public class Pawn extends ChessPiece {
 			p.setValues(position.getRow() + 1, position.getColumn() + 1);
 			if(getBoard().positionExists(p) && isThereOpponentPiece(p))
 				m[p.getRow()][p.getColumn()] = true;
+			
+			// #specialmove en passant black
+			if(position.getRow() == 4) {
+				Position left = new Position(position.getRow(), position.getColumn() - 1);
+				if(getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().getPiece(left) == match.getEnPassant())
+					m[left.getRow() + 1][left.getColumn()] = true;
+				
+				Position right = new Position(position.getRow(), position.getColumn() + 1);
+				if(getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().getPiece(right) == match.getEnPassant())
+					m[right.getRow() + 1][right.getColumn()] = true;
+			}
 		}
 		return m;
 	}
